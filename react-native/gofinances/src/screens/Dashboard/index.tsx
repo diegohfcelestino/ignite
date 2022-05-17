e53855c0016fa1214e5190fc066b1eb9efa8ffca
diegohfcelestino <diegohfcelestino@gmail.com>
2022-05-17 17:10:42 -0300

    Criando components de cards

---
+++ react-native/gofinances/src/screens/Dashboard/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { HighlightCard } from '../../components/HighlightCard'
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  Icon,
+  HighlightCards
+} from './styles'
+
+export function Dashboard() {
+  return (
+    <Container>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{ uri: 'https://github.com/diegohfcelestino.png' }}
+            />
+            <User>
+              <UserGreeting>Olá, </UserGreeting>
+              <UserName>Diego</UserName>
+            </User>
+          </UserInfo>
+          <Icon name="power" />
+        </UserWrapper>
+      </Header>
+      <HighlightCards>
+        <HighlightCard />
+        <HighlightCard />
+        <HighlightCard />
+      </HighlightCards>
+    </Container>
+  )
+}
