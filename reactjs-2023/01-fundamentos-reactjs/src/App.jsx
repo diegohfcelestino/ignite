a33d76fc84cf7066a3dd7b474ac73a448b05e925
diegohfcelestino <diegohfcelestino@gmail.com>
2023-03-27 19:46:51 -0300

    Entendendo o css modules, para não ter inteferencia de estilos na nossa aplicação

--- reactjs-2023/01-fundamentos-reactjs/src/App.jsx
+++ reactjs-2023/01-fundamentos-reactjs/src/App.jsx
@@ -1,8 +1,11 @@
+import { Header } from './components/Header';
 import { Post } from './Post';
+import './styles.css';
 
 export function App() {
   return (
     <div>
+      <Header />
       <Post
         author="Diego Ferreira"
         content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae placeat sed ab sunt voluptatem, natus commodi ex at eos sint dolorum! Non, qui recusandae. Aut molestiae error ipsa quae debitis."
