8bf4a908a0e734bd647a79ddfd7dd6df9fe85ebb
diegohfcelestino <diegohfcelestino@gmail.com>
2023-03-27 19:33:13 -0300

    Entendendo as propriedades e aplicando na pratica

--- reactjs-2023/01-fundamentos-reactjs/src/App.jsx
+++ reactjs-2023/01-fundamentos-reactjs/src/App.jsx
@@ -2,12 +2,12 @@ import { Post } from './Post';
 
 export function App() {
   return (
-    <>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-    </>
+    <div>
+      <Post
+        author="Diego Ferreira"
+        content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae placeat sed ab sunt voluptatem, natus commodi ex at eos sint dolorum! Non, qui recusandae. Aut molestiae error ipsa quae debitis."
+      />
+      <Post author="Gabriel Buzzi" content="Um novo post muito legal" />
+    </div>
   );
 }
