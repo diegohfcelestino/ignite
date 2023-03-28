7edd8455e87e2272acdf9951ac8c96816a654dae
diegohfcelestino <diegohfcelestino@gmail.com>
2023-03-28 18:58:35 -0300

    Iniciando o Sidebar

--- reactjs-2023/01-fundamentos-reactjs/src/App.jsx
+++ reactjs-2023/01-fundamentos-reactjs/src/App.jsx
@@ -1,16 +1,24 @@
 import { Header } from './components/Header';
 import { Post } from './Post';
+import styles from './App.module.css';
 import './global.css';
+import { Sidebar } from './components/Sidebar';
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post
-        author="Diego Ferreira"
-        content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae placeat sed ab sunt voluptatem, natus commodi ex at eos sint dolorum! Non, qui recusandae. Aut molestiae error ipsa quae debitis."
-      />
-      <Post author="Gabriel Buzzi" content="Um novo post muito legal" />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Diego Ferreira"
+            content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae placeat sed ab sunt voluptatem, natus commodi ex at eos sint dolorum! Non, qui recusandae. Aut molestiae error ipsa quae debitis."
+          />
+          <Post author="Gabriel Buzzi" content="Um novo post muito legal" />
+        </main>
+      </div>
     </div>
   );
 }
