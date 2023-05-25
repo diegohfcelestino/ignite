d03bcd5f24c6e9d74a3aa8745cdae394238a4a22
diegohfcelestino <diegohfcelestino@gmail.com>
2023-05-25 18:20:23 -0300

    Ajustando fontes, status bar e component de loading

--- react-native-2023/2-ignite-teams/App.tsx
+++ react-native-2023/2-ignite-teams/App.tsx
@@ -1,9 +1,20 @@
+import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold
+} from '@expo-google-fonts/roboto';
 import theme from './src/theme';
 import { Groups } from '@screens/Groups';
-import { StatusBar } from 'react-native';
+import { Loading } from '@components/Loading';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  });
+
   return (
     <ThemeProvider theme={theme}>
       <StatusBar
@@ -11,7 +22,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      <Groups />
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
