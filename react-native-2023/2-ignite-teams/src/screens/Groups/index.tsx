ed4a5bee170e414260d47ce01dcb4167ac3ecf76
diegohfcelestino <diegohfcelestino@gmail.com>
2023-05-25 19:57:06 -0300

    Criando componente de titulo e subtitulo

--- react-native-2023/2-ignite-teams/src/screens/Groups/index.tsx
+++ react-native-2023/2-ignite-teams/src/screens/Groups/index.tsx
@@ -1,11 +1,13 @@
 import { Header } from '@components/Header';
 import { Container } from './styles';
+import { Highlight } from '@components/Highlight';
 // import * as S from './styles';
 
 export function Groups() {
   return (
     <Container>
       <Header />
+      <Highlight title="Turmas" subtitle="Jogue com a sua turma" />
     </Container>
   );
 }
