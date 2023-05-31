165fe9781dfc5f192f1572628ee8d71e84d6fbcc
diegohfcelestino <diegohfcelestino@gmail.com>
2023-05-31 20:23:31 -0300

    Finalizando a lista de jogadores

--- react-native-2023/2-ignite-teams/src/screens/Players/index.tsx
+++ react-native-2023/2-ignite-teams/src/screens/Players/index.tsx
@@ -4,13 +4,25 @@ import { Highlight } from '@components/Highlight';
 import { ButtonIcon } from '@components/ButtonIcon';
 import { Input } from '@components/Input';
 import { Filter } from '@components/Filter';
+import { PlayerCard } from '@components/PlayerCard';
+import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
 import { FlatList } from 'react-native';
 import { useState } from 'react';
-import { PlayerCard } from '@components/PlayerCard';
 
 export function Players() {
   const [team, setTeam] = useState('Time A');
-  const [players, setPlayers] = useState(['Diego', 'Henrique', 'Ferreira']);
+  const [players, setPlayers] = useState([
+    'Diego',
+    'Henrique',
+    'Ferreira',
+    'Ana',
+    'Jorge',
+    'Michael',
+    'Lucas',
+    'Gilmar',
+    'Josias'
+  ]);
   return (
     <Container>
       <Header showBackButton />
@@ -43,7 +55,16 @@ export function Players() {
         renderItem={({ item }) => (
           <PlayerCard name={item} onRemove={() => {}} />
         )}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Não há pessoas nesse time" />
+        )}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={[
+          { paddingBottom: 100 },
+          players.length === 0 && { flex: 1 }
+        ]}
       />
+      <Button title="Remover Turma" type="SECONDARY" />
     </Container>
   );
 }
