110d1958373c32a4aa3c90fd829eb135ec9c3bd2
diegohfcelestino <diegohfcelestino@gmail.com>
2023-05-31 20:14:55 -0300

    criando componente de listagem dos players

--- react-native-2023/2-ignite-teams/src/screens/Players/index.tsx
+++ react-native-2023/2-ignite-teams/src/screens/Players/index.tsx
@@ -6,10 +6,11 @@ import { Input } from '@components/Input';
 import { Filter } from '@components/Filter';
 import { FlatList } from 'react-native';
 import { useState } from 'react';
+import { PlayerCard } from '@components/PlayerCard';
 
 export function Players() {
   const [team, setTeam] = useState('Time A');
-  const [players, setPlayers] = useState(['Diego', 'Henrique']);
+  const [players, setPlayers] = useState(['Diego', 'Henrique', 'Ferreira']);
   return (
     <Container>
       <Header showBackButton />
@@ -36,6 +37,13 @@ export function Players() {
         />
         <NumbersOfPlayers>{players.length}</NumbersOfPlayers>
       </HeaderList>
+      <FlatList
+        data={players}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <PlayerCard name={item} onRemove={() => {}} />
+        )}
+      />
     </Container>
   );
 }
