6e3049345ec7c112136539e8e001dfed8a017f05
diegohfcelestino <diegohfcelestino@gmail.com>
2023-05-31 21:53:09 -0300

    NAvegando pela aplicação

--- react-native-2023/2-ignite-teams/src/screens/Groups/index.tsx
+++ react-native-2023/2-ignite-teams/src/screens/Groups/index.tsx
@@ -1,15 +1,22 @@
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import { Header } from '@components/Header';
 import { Container } from './styles';
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
 import { ListEmpty } from '@components/ListEmpty';
-import { useState } from 'react';
-import { FlatList } from 'react-native';
 import { Button } from '@components/Button';
 // import * as S from './styles';
 
-export function Groups() {
+export function Groups({}) {
   const [groups, setGroups] = useState<string[]>(['Galera da Rocket']);
+  const navigation = useNavigation();
+
+  function handleNewGroup() {
+    navigation.navigate('new');
+  }
+
   return (
     <Container>
       <Header />
@@ -23,7 +30,7 @@ export function Groups() {
           <ListEmpty message="Que tal cadastrar a primeira turma?" />
         )}
       />
-      <Button title="Criar nova turma" />
+      <Button title="Criar nova turma" onPress={handleNewGroup} />
     </Container>
   );
 }
