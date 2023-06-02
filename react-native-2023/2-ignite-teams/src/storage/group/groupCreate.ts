e74b963094efacab2679a833e88309417b8e50f6
diegohfcelestino <diegohfcelestino@gmail.com>
2023-06-02 20:05:12 -0300

    Criando classe de erro personalizada

--- react-native-2023/2-ignite-teams/src/storage/group/groupCreate.ts
+++ react-native-2023/2-ignite-teams/src/storage/group/groupCreate.ts
@@ -2,10 +2,17 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import { GROUP_COLLECTION } from '@storage/storageConfig';
 import { groupsGetAll } from './groupsGetAll';
+import { AppError } from '@utils/AppError';
 
 export async function groupCreate(newGroup: string) {
   try {
     const storedGroups = await groupsGetAll();
+    //Abaixo verifica se já existe o grupo no storage
+    const groupAlreadyExists = storedGroups.includes(newGroup);
+
+    if (groupAlreadyExists) {
+      throw new AppError('Já existe um grupo cadastrado com este nome.');
+    }
 
     const storage = JSON.stringify([...storedGroups, newGroup]);
 
