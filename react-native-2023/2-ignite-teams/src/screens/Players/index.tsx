423a968d98d6d112613c5acddb328ff12fd3e8c8
diegohfcelestino <diegohfcelestino@gmail.com>
2023-06-02 22:48:26 -0300

    Refatorando o loading

--- react-native-2023/2-ignite-teams/src/screens/Players/index.tsx
+++ react-native-2023/2-ignite-teams/src/screens/Players/index.tsx
@@ -64,13 +64,14 @@ export function Players() {
     try {
       const playersByTeam = await playerGetByGroupAndTeam(group, team);
       setPlayers(playersByTeam);
-      setIsLoading(false);
     } catch (error) {
       console.log(error);
       Alert.alert(
         'Pessoas',
         'Não foi possível carregar as pessoas filtradas do time selecionado'
       );
+    } finally {
+      setIsLoading(false);
     }
   }
 
