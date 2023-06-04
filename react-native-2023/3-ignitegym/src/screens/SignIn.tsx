0b45ef0d27d13121de1c7dd9e8656c68ae7440fa
diegohfcelestino <diegohfcelestino@gmail.com>
2023-06-03 22:51:35 -0300

    Finalizando o input

--- react-native-2023/3-ignitegym/src/screens/SignIn.tsx
+++ react-native-2023/3-ignitegym/src/screens/SignIn.tsx
@@ -5,7 +5,7 @@ import BackgroundImg from '@assets/background.png';
 import { Input } from '@components/Input';
 export function SignIn() {
   return (
-    <VStack flex={1} bg="gray.700">
+    <VStack flex={1} bg="gray.700" px={10}>
       <Image
         source={BackgroundImg}
         alt="Pessoas treinando"
@@ -22,8 +22,12 @@ export function SignIn() {
         <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
           Acesse sua conta
         </Heading>
-        <Input placeholder="E-mail" />
-        <Input placeholder="Senha" />
+        <Input
+          placeholder="E-mail"
+          keyboardType="email-address"
+          autoCapitalize="none"
+        />
+        <Input placeholder="Senha" secureTextEntry />
       </Center>
     </VStack>
   );
