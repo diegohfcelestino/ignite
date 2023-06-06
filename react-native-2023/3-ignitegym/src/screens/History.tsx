f7d37db90578986a62003bb3aa377869c1dcfb9e
diegohfcelestino <diegohfcelestino@gmail.com>
2023-06-05 21:20:30 -0300

    Finalizando o histórico de exercícios

--- react-native-2023/3-ignitegym/src/screens/History.tsx
+++ react-native-2023/3-ignitegym/src/screens/History.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { ScreenHeader } from '@components/ScreenHeader';
 import { HistoryCard } from '@components/HistoryCard';
-import { Heading, VStack, SectionList } from 'native-base';
+import { Heading, VStack, SectionList, Text } from 'native-base';
 
 export function History() {
   const [exercises, setExercises] = useState([
@@ -28,6 +28,15 @@ export function History() {
           </Heading>
         )}
         px={8}
+        contentContainerStyle={
+          exercises.length === 0 && { flex: 1, justifyContent: 'center' }
+        }
+        ListEmptyComponent={() => (
+          <Text color="gray.100" textAlign="center">
+            Não há exercícios registrados ainda. {'\n'}
+            Vamos fazer exercícios hoje?
+          </Text>
+        )}
       />
     </VStack>
   );
