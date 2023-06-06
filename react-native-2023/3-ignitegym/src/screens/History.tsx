f12c9632c3ec8028fb887d53663ba828298a0523
diegohfcelestino <diegohfcelestino@gmail.com>
2023-06-05 21:14:29 -0300

    Utilizando a sectionList

--- react-native-2023/3-ignitegym/src/screens/History.tsx
+++ react-native-2023/3-ignitegym/src/screens/History.tsx
@@ -1,14 +1,34 @@
+import { useState } from 'react';
 import { ScreenHeader } from '@components/ScreenHeader';
 import { HistoryCard } from '@components/HistoryCard';
-import { VStack } from 'native-base';
+import { Heading, VStack, SectionList } from 'native-base';
 
 export function History() {
+  const [exercises, setExercises] = useState([
+    {
+      title: '26.08.23',
+      data: ['Puxada', 'Burger', 'Risotto']
+    },
+    {
+      title: '29.08.23',
+      data: ['French Fries']
+    }
+  ]);
   return (
     <VStack flex={1}>
       <ScreenHeader title="Histórico de Exercícios" />
-      <HistoryCard />
-      <HistoryCard />
-      <HistoryCard />
+
+      <SectionList
+        sections={exercises}
+        keyExtractor={item => item}
+        renderItem={({ item }) => <HistoryCard />}
+        renderSectionHeader={({ section }) => (
+          <Heading color="gray.200" fontSize="md" mt={10} mb={3}>
+            {section.title}
+          </Heading>
+        )}
+        px={8}
+      />
     </VStack>
   );
 }
