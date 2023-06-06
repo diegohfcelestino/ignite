dace345e182b80670a71e735272b50ee138e80db
diegohfcelestino <diegohfcelestino@gmail.com>
2023-06-05 21:47:32 -0300

    Finalizando tela de perfil

--- react-native-2023/3-ignitegym/src/screens/Profile.tsx
+++ react-native-2023/3-ignitegym/src/screens/Profile.tsx
@@ -1,7 +1,17 @@
 import { useState } from 'react';
 import { ScreenHeader } from '@components/ScreenHeader';
 import { UserPhoto } from '@components/UserPhoto';
-import { Center, ScrollView, Skeleton, VStack } from 'native-base';
+import { Input } from '@components/Input';
+import { Button } from '@components/Button';
+import {
+  Center,
+  Heading,
+  ScrollView,
+  Skeleton,
+  Text,
+  VStack
+} from 'native-base';
+import { TouchableOpacity } from 'react-native';
 const PHOTO_SIZE = 33;
 
 export function Profile() {
@@ -26,7 +36,33 @@ export function Profile() {
               size={PHOTO_SIZE}
             />
           )}
+          <TouchableOpacity>
+            <Text
+              color="green.500"
+              fontWeight="bold"
+              fontSize="md"
+              mt={2}
+              mb={8}
+            >
+              Alterar foto
+            </Text>
+          </TouchableOpacity>
+          <Input placeholder="Nome" bg="gray.600" />
+          <Input placeholder="E-mail" bg="gray.600" isDisabled />
         </Center>
+        <VStack px={10} mt={12} mb={9}>
+          <Heading color="gray.200" fontSize="md" mb={2}>
+            Alterar senha
+          </Heading>
+          <Input placeholder="Senha antiga" bg="gray.600" secureTextEntry />
+          <Input placeholder="Nova Senha" bg="gray.600" secureTextEntry />
+          <Input
+            placeholder="Confirme a nova Senha"
+            bg="gray.600"
+            secureTextEntry
+          />
+          <Button title="Atualizar" mt={4} />
+        </VStack>
       </ScrollView>
     </VStack>
   );
