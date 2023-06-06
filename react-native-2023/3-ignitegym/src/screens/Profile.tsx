9fec502b73b63bf31b0294f9af66fb42c6c62d6d
diegohfcelestino <diegohfcelestino@gmail.com>
2023-06-06 20:56:51 -0300

    Ativando o carregamento da foto

--- react-native-2023/3-ignitegym/src/screens/Profile.tsx
+++ react-native-2023/3-ignitegym/src/screens/Profile.tsx
@@ -22,18 +22,27 @@ export function Profile() {
   );
 
   async function handleUserPhotoSelect() {
-    const photoSelected = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
-      quality: 1,
-      aspect: [4, 4],
-      allowsEditing: true
-      // base64: true
-    });
+    setPhotoIsLoading(true);
+    try {
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4, 4],
+        allowsEditing: true
+        // base64: true
+      });
 
-    if (photoSelected.canceled) {
-      return;
+      if (photoSelected.canceled) {
+        return;
+      }
+      if (photoSelected.assets[0].uri) {
+        setUserPhoto(photoSelected.assets[0].uri);
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setPhotoIsLoading(false);
     }
-    setUserPhoto(photoSelected.assets[0].uri);
   }
 
   return (
