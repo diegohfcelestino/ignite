8bfdfbbb7ba0d174a43cbb36fdccaf7ceae101f5
diegohfcelestino <diegohfcelestino@gmail.com>
2023-06-05 21:01:58 -0300

    Criando componente de histórico de exercícios

--- react-native-2023/3-ignitegym/src/screens/History.tsx
+++ react-native-2023/3-ignitegym/src/screens/History.tsx
@@ -1,10 +1,14 @@
 import { ScreenHeader } from '@components/ScreenHeader';
+import { HistoryCard } from '@components/HistoryCard';
 import { VStack } from 'native-base';
 
 export function History() {
   return (
     <VStack flex={1}>
       <ScreenHeader title="Histórico de Exercícios" />
+      <HistoryCard />
+      <HistoryCard />
+      <HistoryCard />
     </VStack>
   );
 }
