479d6fe02d39d5df862976a5e0fc4834b73e1bce
diegohfcelestino <diegohfcelestino@gmail.com>
2023-06-05 21:35:00 -0300

    Utilizando o Skeleton

--- react-native-2023/3-ignitegym/src/screens/Profile.tsx
+++ react-native-2023/3-ignitegym/src/screens/Profile.tsx
@@ -1,18 +1,31 @@
+import { useState } from 'react';
 import { ScreenHeader } from '@components/ScreenHeader';
 import { UserPhoto } from '@components/UserPhoto';
-import { Center, ScrollView, Text, VStack } from 'native-base';
+import { Center, ScrollView, Skeleton, VStack } from 'native-base';
+const PHOTO_SIZE = 33;
 
 export function Profile() {
+  const [photoIsLoading, setPhotoIsLoading] = useState(false);
   return (
     <VStack flex={1}>
       <ScreenHeader title="Perfil" />
       <ScrollView>
         <Center mt={6} px={10}>
-          <UserPhoto
-            source={{ uri: 'https://github.com/diegohfcelestino.png' }}
-            alt="Imagem do usuário"
-            size={33}
-          />
+          {photoIsLoading ? (
+            <Skeleton
+              w={PHOTO_SIZE}
+              h={PHOTO_SIZE}
+              rounded="full"
+              startColor="gray.500"
+              endColor="gray.400"
+            />
+          ) : (
+            <UserPhoto
+              source={{ uri: 'https://github.com/diegohfcelestino.png' }}
+              alt="Imagem do usuário"
+              size={PHOTO_SIZE}
+            />
+          )}
         </Center>
       </ScrollView>
     </VStack>
