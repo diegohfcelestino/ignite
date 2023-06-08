5f8b9562e8d1c43dd677ab147d1cf29dacb84972
diegohfcelestino <diegohfcelestino@gmail.com>
2023-06-08 15:59:01 -0300

    Validando campos de senha e confirma senha

--- react-native-2023/3-ignitegym/src/screens/SignUp.tsx
+++ react-native-2023/3-ignitegym/src/screens/SignUp.tsx
@@ -18,8 +18,16 @@ type FormDataProps = {
 };
 
 const signUpSchema = yup.object({
-  name: yup.string().required('Informe o nome'),
-  email: yup.string().required('Informe o e-mail').email('E-mail inválido')
+  name: yup.string().required('Informe o nome.'),
+  email: yup.string().required('Informe o e-mail.').email('E-mail inválido.'),
+  password: yup
+    .string()
+    .required('Informe a senha.')
+    .min(6, 'A senha deve ter pelo menos 6 dígitos.'),
+  password_confirm: yup
+    .string()
+    .required('Confirme a senha')
+    .oneOf([yup.ref('password'), null], 'A confirmação de senha não confere')
 });
 
 export function SignUp() {
@@ -107,6 +115,7 @@ export function SignUp() {
                 secureTextEntry
                 onChangeText={onChange}
                 value={value}
+                errorMessage={errors.password?.message}
               />
             )}
           />
@@ -122,6 +131,7 @@ export function SignUp() {
                 value={value}
                 onSubmitEditing={handleSubmit(handleSingnUp)}
                 returnKeyType="send"
+                errorMessage={errors.password_confirm?.message}
               />
             )}
           />
