5c3424bde7217fedc175a3ff668a8a71f67498ff
diegohfcelestino <diegohfcelestino@gmail.com>
2023-06-19 21:36:02 -0300

    Carregando loadin até setar o usuáro na aplicação

--- react-native-2023/3-ignitegym/src/contexts/AuthContext.tsx
+++ react-native-2023/3-ignitegym/src/contexts/AuthContext.tsx
@@ -7,6 +7,7 @@ import { storageUserSave, storageUserGet } from '@storage/storageUser';
 export type AuthContextDataProps = {
   user: UserDTO;
   signIn: (email: string, password: string) => Promise<void>;
+  isLoadingUserStorageData: boolean;
 };
 
 type AuthContextProviderProps = {
@@ -19,6 +20,8 @@ export const AuthContext = createContext<AuthContextDataProps>(
 
 export function AuthContextProvider({ children }: AuthContextProviderProps) {
   const [user, setUser] = useState<UserDTO>({} as UserDTO);
+  const [isLoadingUserStorageData, setIsLoadingUserStorageData] =
+    useState(true);
 
   async function signIn(email: string, password: string) {
     try {
@@ -33,10 +36,16 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
   }
 
   async function loadUserData() {
-    const userLogged = await storageUserGet();
+    try {
+      const userLogged = await storageUserGet();
 
-    if (userLogged) {
-      setUser(userLogged);
+      if (userLogged) {
+        setUser(userLogged);
+      }
+    } catch (error) {
+      throw error;
+    } finally {
+      setIsLoadingUserStorageData(false);
     }
   }
 
@@ -44,7 +53,7 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
     loadUserData();
   }, []);
   return (
-    <AuthContext.Provider value={{ user, signIn }}>
+    <AuthContext.Provider value={{ isLoadingUserStorageData, user, signIn }}>
       {children}
     </AuthContext.Provider>
   );
