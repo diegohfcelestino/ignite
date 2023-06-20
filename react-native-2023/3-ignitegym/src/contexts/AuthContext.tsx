a4372c9446320967013eb7600c468fecd6b0fc58
diegohfcelestino <diegohfcelestino@gmail.com>
2023-06-19 21:24:10 -0300

    Persistindo dados do usuário no dispositivo e recuperando os dados no async storage

--- react-native-2023/3-ignitegym/src/contexts/AuthContext.tsx
+++ react-native-2023/3-ignitegym/src/contexts/AuthContext.tsx
@@ -1,6 +1,8 @@
 import { UserDTO } from '@dtos/UserDTO';
 import { api } from '@services/api';
-import { ReactNode, createContext, useState } from 'react';
+import { ReactNode, createContext, useEffect, useState } from 'react';
+
+import { storageUserSave, storageUserGet } from '@storage/storageUser';
 
 export type AuthContextDataProps = {
   user: UserDTO;
@@ -23,11 +25,24 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
       const { data } = await api.post('/sessions', { email, password });
       if (data.user) {
         setUser(data.user);
+        storageUserSave(data.user);
       }
     } catch (error) {
       throw error;
     }
   }
+
+  async function loadUserData() {
+    const userLogged = await storageUserGet();
+
+    if (userLogged) {
+      setUser(userLogged);
+    }
+  }
+
+  useEffect(() => {
+    loadUserData();
+  }, []);
   return (
     <AuthContext.Provider value={{ user, signIn }}>
       {children}
