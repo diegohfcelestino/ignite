c12eb9a34f26de586fb5bad7b1d37918f556f012
diegohfcelestino <diegohfcelestino@gmail.com>
2024-02-28 22:39:18 -0300

    Utilizando stream na pratica

--- nodejs-2024/01-fundamentos-nodejs/src/server.js
+++ nodejs-2024/01-fundamentos-nodejs/src/server.js
@@ -14,26 +14,41 @@ import http from 'node:http';
 
 const users = [];
 
-const server = http.createServer((request, response) => {
-  const { method, url } = request;
+const server = http.createServer(async (req, res) => {
+  const { method, url } = req;
 
-  console.log(request.headers);
+  const buffers = [];
+
+  for await (const chunk of req) {
+    buffers.push(chunk);
+  }
+
+  try {
+    req.body = JSON.parse(Buffer.concat(buffers).toString());
+  } catch {
+    req.body = null;
+  }
+
+  console.log(req.body);
 
   if (method === 'GET' && url === '/users') {
-    return response
+    return res
       .setHeader('Content-type', 'application/json')
       .end(JSON.stringify(users));
   }
 
   if (method === 'POST' && url === '/users') {
+
+    const { name, email } = req.body;
+
     users.push({
       id: 1,
-      name: 'John Doe',
-      email: 'johndoe@exemple.com'
+      name,
+      email
     });
-    return response.writeHead(201).end();
+    return res.writeHead(201).end();
   }
-  return response.writeHead(404).end("Not Found");
+  return res.writeHead(404).end("Not Found");
 });
 
 server.listen(3333);
